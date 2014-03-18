173411a8996a4abd52bc8f5d396ef2931561462a
Yuriy Sanikov <yxs0@nreca.org>
2014-03-18 11:36:44 -0400

    Code coverage attribute added

--- src/NHateoas/src/Configuration/QueryParameter.cs
+++ src/NHateoas/src/Configuration/QueryParameter.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NHateoas.Configuration
 {
+    [ExcludeFromCodeCoverage]
     public static class QueryParameter
     {
         public static TValue Is<TValue>()
